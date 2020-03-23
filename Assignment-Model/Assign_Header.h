e6a17652eeeaf27fd25b4ac65504560e4bf88606
heamchandb <61612483+heamchandb@users.noreply.github.com>
2020-03-23 10:32:30 +0530

    Update Assign_Header.h

--- Assignment-Model/Assign_Header.h
+++ Assignment-Model/Assign_Header.h
@@ -1,7 +1,14 @@
+/*                                               AUM SRI SAI RAM
+Name:HEAM CHAND B.
+Regd.no:173202.
+Dste:23-03-2020.
+*/
+//This file contains the functions that are defined in header file to solve 'THE ASSIGNMENT MODEL' problem using "HUNGARIAN METHOD".
+
 #include <stdio.h>
 #include <stdlib.h>
 
-static int **Array;
+static int **Array;                  
 static int **Result;
 FILE *f;
 
@@ -10,4 +17,3 @@ void hungarian();
 void finalSol(int cost);
 void doubleCheck(int row_dec[],int col_inc[],int col_mate[],int cost);
 void nl(int k);
-
