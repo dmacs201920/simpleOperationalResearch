6d29b1043561a5c0b0f2d04df5175394acf973a4
heamchandb <61612483+heamchandb@users.noreply.github.com>
2020-03-23 10:38:24 +0530

    Update Simplex_Header.h

--- Simplex/Simplex_Header.h
+++ Simplex/Simplex_Header.h
@@ -1,3 +1,9 @@
+/*                                               AUM SRI SAI RAM
+Name:HEAM CHAND B & P S SRUJAN
+Regd.no:173202, 171207
+Dste:23-03-2020.
+*/
+//This file is the header file to solve 'LPP' problem using "SIMPLEX METHOD".
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
